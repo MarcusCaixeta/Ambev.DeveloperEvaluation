ddd164829f5b87b2b87e502436959805cbcf1f79
MarcusCaixeta <marcus.caixeta@hotmail.com>
2025-03-30 07:01:28 -0300

    feat(sale) DeleteSale - Implementado DeleteSale.

--- src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
@@ -21,6 +21,7 @@ namespace Ambev.DeveloperEvaluation.Domain.Repositories
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>The sale if found, null otherwise</returns>
         Task<Sale?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
         /// <summary>
         /// Creates a new sale in the repository
         /// </summary>
@@ -28,5 +29,13 @@ namespace Ambev.DeveloperEvaluation.Domain.Repositories
         /// <param name="cancellationToken">Cancellation token</param>
         /// <returns>The created sale</returns>
         Task<Sale> CreateAsync(Sale sale, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes a sale from the repository
+        /// </summary>
+        /// <param name="id">The unique identifier of the sale to delete</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>True if the sale was deleted, false if not found</returns>
+        Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
     }
 }
--- src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SalesController.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SalesController.cs
@@ -1,8 +1,12 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
 using Ambev.DeveloperEvaluation.Application.Sales.GetSaleById;
+using Ambev.DeveloperEvaluation.Application.Users.DeleteUser;
 using Ambev.DeveloperEvaluation.WebApi.Common;
 using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.DeleteSale;
 using Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSaleById;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.DeleteUser;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -86,5 +90,34 @@ public class SalesController : BaseController
             Message = "Sale retrieved successfully",
             Data = _mapper.Map<GetSaleByIdResponse>(response)
         });
-    }    
+    }
+
+    /// <summary>
+    /// Deletes a user by their ID
+    /// </summary>
+    /// <param name="id">The unique identifier of the user to delete</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>Success response if the user was deleted</returns>
+    [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteUser([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var request = new DeleteSaleRequest { Id = id };
+        var validator = new DeleteSaleRequestValidator();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+        if (!validationResult.IsValid)
+            return BadRequest(validationResult.Errors);
+
+        var command = _mapper.Map<DeleteSaleCommand>(request.Id);
+        await _mediator.Send(command, cancellationToken);
+
+        return Ok(new ApiResponse
+        {
+            Success = true,
+            Message = "Sale deleted successfully"
+        });
+    }
 }
